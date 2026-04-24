cc98e11c2f93039392d24268032d3214c71bb8ba
Murilo Santana Novais <95980479+MuriloSnovais@users.noreply.github.com>
2026-04-24 10:57:09 -0300

    Modificando services.py

--- services.py
+++ services.py
@@ -1,6 +1,17 @@
 import json
+from collections import Counter
 
-with open('logs.json','r') as archive:
-    for line in archive:
-        print(line.strip())
+def read_logs():
+    with open('logs.json','r', encoding='utf-8') as archive:
+        data = json.load(archive)
+        for info_logs in data:
+            print(f'IP: {info_logs['ip']} | Login: {info_logs['login']}')
+              
+
+read_logs()
+
+    
+
+
+
 
