62bfcb0a8c67436d5be57136fe3ad8d1b6a1293f
Murilo Santana Novais <95980479+MuriloSnovais@users.noreply.github.com>
2026-05-02 21:24:53 -0300

    Add pydantic ao codigo

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 from services import suspect_logs
-from fastapi import Body
+from pydantic import BaseModel
+from typing import Dict
 
 app = FastAPI()
+
+class logBase(BaseModel):
+    ip: str
+    login: str
 
 @app.get("/")
 def log():
     return {"status": "API online"}
     
 @app.post("/analise")
-def take_log(dados: list = Body(...)):
+def take_log(dados: list[logBase]):
     return suspect_logs(dados)
     
--- services.py
+++ services.py
@@ -1,13 +1,12 @@
-import json
 from collections import Counter
 
-def suspect_logs(dados_revebidos):
+def suspect_logs(receive_data):
     suspect_ips = []
     final_ips = []
 
-    for info_logs in dados_revebidos:
-        if info_logs['login'] == 'failed':
-            suspect_ips.append(info_logs['ip'])
+    for info_logs in receive_data:
+        if info_logs.login == 'failed':
+            suspect_ips.append(info_logs.ip)
 
     count = Counter(suspect_ips)
     for ips,times in count.items():
